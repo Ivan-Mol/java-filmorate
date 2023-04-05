1d62ab2c8af52687ce9a17a09dbfa9a8dd6525ae
Ivan-Mol <abyss.devotee@gmail.com>
2023-04-05 13:26:39 +0200

    checkstyle fixes

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -2,9 +2,18 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.service.ReviewService;
+
 import javax.validation.Valid;
 import java.util.List;
 
@@ -14,6 +23,7 @@ import java.util.List;
 @Slf4j
 public class ReviewController {
     private final ReviewService reviewService;
+
     @GetMapping("/{id}")
     public Review getReviewById(@PathVariable int id) {
         log.debug("/getReviewById");
@@ -49,6 +59,7 @@ public class ReviewController {
         reviewService.removeReview(id);
         log.debug("Review with id={} was deleted", id);
     }
+
     @PutMapping("/{id}/like/{userId}")
     public void addLikeToReview(@PathVariable int id, @PathVariable int userId) {
         log.debug("/addLikeToReview");
