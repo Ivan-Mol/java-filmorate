ae1c7ba04446332ab8af095409a92688f7ea1aff
KEV-work <e.u.kuznetsov@yandex.ru>
2023-04-06 18:06:08 +0300

    fix: schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,4 @@
---DROP ALL OBJECTS DELETE FILES;
-DROP TABLE IF EXISTS mpa, films, genres, users, film_genres, likes, friends, reviews, review_like_dislike, directors, film_directors, events CASCADE;
+DROP ALL OBJECTS DELETE FILES;
 
 create TABLE IF NOT EXISTS mpa
 (
